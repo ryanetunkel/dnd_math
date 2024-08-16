fcd1cb335b3a3bc46cfc8c8b923030584dfec007
ryanetunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-08-16 16:33:31 -0500

    Add class_type enum

---
+++ Rainbow Warrior/class_type.py
@@ -0,0 +1,15 @@
+"""The Class_Type Class"""
+
+from enum import Enum
+
+class Class_Type(Enum):
+    NONE = 0
+    ARTIFICER = 1
+    BARD = 2
+    BLOOD_HUNTER = 3
+    CLERIC = 4
+    DRUID = 5
+    FIGHTER = 6
+    PALADIN = 7
+    RANGER = 8
+    WARLOCK = 9
