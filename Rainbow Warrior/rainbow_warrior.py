a74eb90153999c90b478b4c638683f990f33b7e5
ryanetunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-08-16 17:34:06 -0500

    Progress on rainbow warrior

--- Rainbow Warrior/rainbow_warrior.py
+++ Rainbow Warrior/rainbow_warrior.py
@@ -4,8 +4,8 @@
 
 from pprint import pprint
 
-from damage import Damage
-
+from damage_type import Damage_Type
+# from dnd_class import Dnd_Class
 
 source_types = [
     "Background",
@@ -31,27 +31,47 @@ damage_types = [
 ]
 
 mockup_sources = {
-    "Background": {"Strike of the Giants": Damage.LIGHTNING},
-    "Cantrip": ["Booming Blade", Damage.THUNDER],
+    "Background": {"Strike of the Giants": (Damage_Type.LIGHTNING)},
+    "Cantrip": {"Booming Blade": (Damage_Type.THUNDER)},
     "Classes": {
-        "Warlock": ["Genie", 1, [Damage.BLUDGEONING]],
-        "Blood Hunter": ["", 2, [Damage.COLD]],
-        "Bard": ["Whispers", 4, [Damage.PSYCHIC]], # Listed as 4 as will be increased by 1 from feat
-        "Ranger": ["Swarmkeeper", 4, [Damage.PIERCING]], # Listed as 4 as will be increased by 1 from feat
-        "Fighter": ["Rune", 3, [Damage.FIRE]],
+        "Warlock": ("Genie", (Damage_Type.BLUDGEONING), 1),
+        "Blood Hunter": ("", (Damage_Type.COLD), 2),
+        "Bard": ("Whispers", (Damage_Type.PSYCHIC), 4), # Listed as 4 as will be increased by 1 from feat
+        "Ranger": ("Swarmkeeper", (Damage_Type.PIERCING), 4), # Listed as 4 as will be increased by 1 from feat
+        "Fighter": ("Rune", (Damage_Type.FIRE), 3),
     },
-    "Concentration": ["Zephyr Strike", Damage.FORCE],
-    "Feats": [["Vital Sacrifice", Damage.NECROTIC], ["Gift of the Chromatic Dragon: Acid", Damage.ACID]],
-    "Species": ["Aasimar: Radiant", Damage.RADIANT],
+    "Concentration": ("Zephyr Strike", Damage_Type.FORCE),
+    "Feats": [
+        ("Vital Sacrifice", (Damage_Type.NECROTIC)),
+        ("Gift of the Chromatic Dragon: Acid", (Damage_Type.ACID)),
+    ],
+    "Species": ("Aasimar: Radiant", (Damage_Type.RADIANT)),
     "Level": 14,
 }
+
+mockup_classes_dict = {
+    "Warlock": ("Genie", (Damage_Type.BLUDGEONING), 1),
+    "Blood Hunter": ("", (Damage_Type.COLD), 2),
+    "Bard": ("Whispers", (Damage_Type.PSYCHIC), 4),
+    "Ranger": ("Swarmkeeper", (Damage_Type.PIERCING), 4),
+    "Fighter": ("Rune", (Damage_Type.FIRE), 3),
+}
+
+
+def add_class_levels(class_dict: dict) -> int:
+    """Adds levels from a given dict of early draft classes"""
+    levels = 0
+    for attributes in class_dict.values():
+        levels += attributes[2]
+    return levels
 
 
 def run():
     print_source_list = False
     print_damage_types_list = False
     print_damage_types_enum = False
-    pprint_mockup_sources_one_dict = True
+    pprint_mockup_sources_one_dict = False
+    add_class_levels_from_mockup_dict = True
 
     # Printing 1st Draft Source List
     if print_source_list:
@@ -68,13 +88,20 @@ def run():
         print("")
 
     # Printing 2nd Draft Damage Types via enum
-    print("Damage Types via list: ")
     if print_damage_types_enum:
-        for damage in Damage:
+        print("Damage Types via enum: ")
+        for damage in Damage_Type:
             print("\t" + str(damage.name) + ": " + str(damage.value))
             print("")
 
+    # Printing Mockup source dict via pprint
     if pprint_mockup_sources_one_dict:
         pprint(mockup_sources)
+
+    # Printing class level total from mockup dict
+    if add_class_levels_from_mockup_dict:
+        levels = add_class_levels(mockup_classes_dict)
+        print(levels)
+
 
 run()
