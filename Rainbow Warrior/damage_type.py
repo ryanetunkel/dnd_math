83ef2607d4e52bc846672ff9772e8ae4ab51d6b6
ryanetunkel <123666075+ryanetunkel@users.noreply.github.com>
2024-08-16 16:33:20 -0500

    Add damage_type enum

---
+++ Rainbow Warrior/damage_type.py
@@ -0,0 +1,17 @@
+"""The Damage_Type Enum"""
+
+from enum import Enum
+
+class Damage_Type(Enum):
+    NONE = 0
+    ACID = 1
+    BLUDGEONING = 2
+    COLD = 3
+    FIRE = 4
+    FORCE = 5
+    LIGHTNING = 6
+    NECROTIC = 7
+    PIERCING = 8
+    PSYCHIC = 9
+    RADIANT = 10
+    THUNDER = 11
